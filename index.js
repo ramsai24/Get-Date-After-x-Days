e9621c90b31b5bf140d4c5d921157e2ddd2facfd
ramsai24 <jnrsai24@gmail.com>
2023-05-06 09:31:56 +0000

    all test cases passed

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 //const addDays = require("date-fsn/addDays");
 const addDays = require("date-fns/addDays");
 
-const result = addDays(new Date(2022, 1, 20), 3);
-console.log(result);
+function date(days) {
+  const givenDay = addDays(new Date(2020, 7, 22), days);
+  const aftersomeDays = `${givenDay.getDate()}-${
+    givenDay.getMonth() + 1
+  }-${givenDay.getFullYear()}`;
+  return aftersomeDays;
+}
 
-// const date = (num_days) => {
-//   const result = addDays(new Date(2022, 1, 20), 3);
-//   return result;
-// };
-// console.log(date());
+console.log(date(10));
+//module.exports = date(10);
+//the above iss wrong statement we should exports only with fucntion name only
+module.exports = date;
